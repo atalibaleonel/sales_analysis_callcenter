86015d287db55900480c716d27a4e0ffd5f1a0d3
Ataliba Leonel <109365478+atalibaleonel@users.noreply.github.com>
2024-02-20 19:39:29 +0000

    app.py

--- app.py
+++ app.py
@@ -5,4 +5,8 @@ FONT_AWESOME = ["https://use.fontawesome.com/releases/v5.10.2/css/all.css"]
 app = dash.Dash(__name__, external_stylesheets=FONT_AWESOME)
 
 app.scripts.config.serve_locally = True
+
 server = app.server
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
